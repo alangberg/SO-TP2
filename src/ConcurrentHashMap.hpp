9a880e00878611afb8b4fc756bb5d1a0675547f4
pato24s <pbsticco@dc.uba.ar>
2017-05-21 16:51:32 -0300

    no compila

---
+++ src/ConcurrentHashMap.hpp
@@ -0,0 +1,12 @@
+#ifndef CONCURRENT_HASHMAP_H__
+#define CONCURRENT_HASHMAP_H__
+#include <atomic>
+
+class ConcurrentHashMap{
+public:
+	ConcurrentHashMap();
+	~ConcurrentHashMap();
+	
+};
+
+#endif /* LISTA_ATOMICA_H__ */
